89e0deb8ef0d403d0d1de2836e9596636817d2b3
EGichuhi <105747015+EGichuhi@users.noreply.github.com>
2026-02-22 22:53:21 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -216,9 +216,9 @@ function App() {
             <div className="bg-[#451e1a] rounded-lg shadow-lg p-8 border border-amber-900/30 transform transition-all duration-300 hover:-translate-y-1 hover:shadow-xl">
               <div className="flex flex-col md:flex-row justify-between mb-4">
                 <h3 className="text-xl font-serif text-amber-100">B2B IT Intern</h3>
-                <p className="text-amber-400">APR 2025 - AUG 2024</p>
+                <p className="text-amber-400">APR 2025 - AUG 2025</p>
               </div>
-              <h4 className="text-lg text-amber-200 mb-2">Telus International, Vancouver, BC (Remote)</h4>
+              <h4 className="text-lg text-amber-200 mb-2">Philip Morris International, Toronto, ON (Hybrid)</h4>
               <ul className="list-disc list-inside space-y-2 text-amber-100">
                 <li>Contributed to system integration initiatives by validating structured data pipelines, ensuring accurate synchronization between ERP systems (SAP) and partner-facing digital tools.</li>
                 <li>Assisted in the development and testing of new features for internal tools (JIRA, Confluence), improving user experience and functionality.</li>
